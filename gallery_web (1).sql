9658ae24428c5b1385e34ae990eb46e72afd8d67
rahmadini11 <rrhmaandini26@gmail.com>
2024-11-24 19:58:14 -0500

    Add files via upload

---
+++ gallery_web (1).sql
@@ -0,0 +1,443 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3310
+-- Generation Time: Nov 25, 2024 at 01:55 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `gallery_web`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `agendas`
+--
+
+CREATE TABLE `agendas` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `description` text DEFAULT NULL,
+  `event_date` datetime DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `agendas`
+--
+
+INSERT INTO `agendas` (`id`, `title`, `description`, `event_date`, `created_at`, `updated_at`) VALUES
+(1, 'p5', 'senam', '2024-10-31 00:00:00', '2024-10-28 07:46:34', '2024-10-28 07:46:34'),
+(2, 'Pentas Seni', 'Mengadakan acara', '2024-01-02 00:00:00', '2024-11-06 16:49:03', '2024-11-06 16:49:03'),
+(3, 'Upacara', 'Upacara Bendera', '2024-04-16 00:00:00', '2024-11-06 16:50:25', '2024-11-06 16:50:25'),
+(4, 'Workshop', 'Kegiatan Rapat Guru', '2024-06-23 00:00:00', '2024-11-06 16:51:07', '2024-11-06 16:51:07');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `comments`
+--
+
+CREATE TABLE `comments` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `photo_id` bigint(20) UNSIGNED NOT NULL,
+  `user_id` bigint(20) UNSIGNED NOT NULL,
+  `content` text NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `comments`
+--
+
+INSERT INTO `comments` (`id`, `photo_id`, `user_id`, `content`, `created_at`, `updated_at`) VALUES
+(1, 1, 3, 'p', '2024-10-28 07:37:54', '2024-10-28 07:37:54'),
+(2, 1, 4, 'p', '2024-11-02 13:48:24', '2024-11-02 13:48:24'),
+(9, 1, 4, 'p', '2024-11-23 21:06:16', '2024-11-23 21:06:16');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `failed_jobs`
+--
+
+CREATE TABLE `failed_jobs` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `uuid` varchar(255) NOT NULL,
+  `connection` text NOT NULL,
+  `queue` text NOT NULL,
+  `payload` longtext NOT NULL,
+  `exception` longtext NOT NULL,
+  `failed_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `galleries`
+--
+
+CREATE TABLE `galleries` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `user_id` bigint(20) UNSIGNED NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `galleries`
+--
+
+INSERT INTO `galleries` (`id`, `title`, `user_id`, `created_at`, `updated_at`) VALUES
+(1, 'lingkungan smkn 4', 1, '2024-10-28 07:28:52', '2024-10-28 07:28:52'),
+(3, 'Ruang Praktik PPLG', 1, '2024-11-04 16:19:09', '2024-11-06 16:46:10'),
+(4, 'Ruang Praktik TPFL', 1, '2024-11-04 16:23:56', '2024-11-04 16:23:56'),
+(5, 'Ruang Praktik TKRO', 1, '2024-11-04 16:25:59', '2024-11-04 16:25:59'),
+(6, 'Ruang Praktik TJKT', 1, '2024-11-04 16:27:12', '2024-11-04 16:27:12');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `infos`
+--
+
+CREATE TABLE `infos` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `content` text NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `infos`
+--
+
+INSERT INTO `infos` (`id`, `title`, `content`, `created_at`, `updated_at`) VALUES
+(4, 'ABM(assesment bakat minat)', 'Senin 11 November 2024', '2024-11-12 10:25:17', '2024-11-12 10:25:41'),
+(5, 'Workshop Guru', 'Selasa 12 November 2024', '2024-11-12 10:26:53', '2024-11-12 10:26:53'),
+(6, 'Kunjungan Industri', '10 Oktober 2024', '2024-11-12 10:27:57', '2024-11-12 10:27:57'),
+(7, 'rar', 'rawr', '2024-11-18 12:16:32', '2024-11-18 12:16:32'),
+(8, 'rar', 'rawr', '2024-11-18 12:17:12', '2024-11-18 12:17:12');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `likes`
+--
+
+CREATE TABLE `likes` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `migrations`
+--
+
+CREATE TABLE `migrations` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `migration` varchar(255) NOT NULL,
+  `batch` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `migrations`
+--
+
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+(1, '2014_10_12_100000_create_password_resets_table', 1),
+(2, '2019_08_19_000000_create_failed_jobs_table', 1),
+(3, '2019_12_14_000001_create_personal_access_tokens_table', 1),
+(4, '2024_09_22_123507_create_users_table', 1),
+(5, '2024_09_22_123533_create_gallery_table', 1),
+(6, '2024_09_22_123551_create_photos_table', 1),
+(7, '2024_09_22_123605_create_comments_table', 1),
+(8, '2024_09_22_123619_create_info_table', 1),
+(9, '2024_09_22_123636_create_agendas_table', 1),
+(10, '2024_09_28_084839_add_title_and_description_to_photos_table', 1),
+(11, '2024_09_28_085511_remove_description_from_galleries_table', 1),
+(12, '2024_11_23_135912_create_likes_table', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `password_resets`
+--
+
+CREATE TABLE `password_resets` (
+  `email` varchar(255) NOT NULL,
+  `token` varchar(255) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `personal_access_tokens`
+--
+
+CREATE TABLE `personal_access_tokens` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `tokenable_type` varchar(255) NOT NULL,
+  `tokenable_id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `token` varchar(64) NOT NULL,
+  `abilities` text DEFAULT NULL,
+  `expires_at` timestamp NULL DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `photos`
+--
+
+CREATE TABLE `photos` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `description` text DEFAULT NULL,
+  `image_url` varchar(255) NOT NULL,
+  `gallery_id` bigint(20) UNSIGNED NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `photos`
+--
+
+INSERT INTO `photos` (`id`, `title`, `description`, `image_url`, `gallery_id`, `created_at`, `updated_at`) VALUES
+(1, 'lapangan', '-', 'https://smkn4bogor.sch.id/assets/images/background/smkn4bogor_2.jpg', 1, '2024-10-28 07:30:50', '2024-10-28 07:30:50'),
+(2, 'Ruang Praktik PPLG', '-', 'https://smkn4bogor.sch.id/assets/images/background/pplg_2.jpg', 3, '2024-11-04 16:19:40', '2024-11-06 16:35:00'),
+(3, 'Ruang Praktik TPFL', '-', 'https://smkn4bogor.sch.id/assets/images/background/tpfl_3.jpg', 4, '2024-11-04 16:24:24', '2024-11-04 16:24:24'),
+(4, 'Ruang Praktik TKRO', '-', 'https://smkn4bogor.sch.id/assets/images/background/tkr.jpeg', 5, '2024-11-04 16:26:27', '2024-11-04 16:26:27'),
+(5, 'Ruang Praktik TJKT', '-', 'https://smkn4bogor.sch.id/assets/images/background/tjkt_3.jpg', 6, '2024-11-04 16:27:35', '2024-11-04 16:27:35');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `email_verified_at` timestamp NULL DEFAULT NULL,
+  `password` varchar(255) NOT NULL,
+  `role` varchar(255) NOT NULL,
+  `remember_token` varchar(100) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `role`, `remember_token`, `created_at`, `updated_at`) VALUES
+(1, 'Admin', 'admin@gmail.com', NULL, '$2y$10$eR8pP4hrC2H79rgtqTGUNObtWx1HtU0n.xhBkG0PK99o6p71m14EG', 'admin', NULL, '2024-10-28 07:26:07', '2024-10-28 07:26:07'),
+(2, 'rahma', 'rahma@gmail.com', NULL, '$2y$10$iRH3nL23h8exjyjNqODDve6gOmZEGSRYr13Md4TOSUNQTtRLA2X8i', 'admin', NULL, '2024-10-28 07:32:32', '2024-10-28 07:33:12'),
+(3, 'user', 'user@gmail.com', NULL, '$2y$10$JdOe.XPLP32zyKSmV6bnkuFCOC/06WcABK0z6djPGQQOkxFWmD17S', 'admin', NULL, '2024-10-28 07:33:59', '2024-11-14 09:44:29'),
+(4, 'rahma', 'rahmaandini@gmail.com', NULL, '$2y$10$L/eY1Y8pSi5EhpIz9NrtMOMCM8XPC80Fk9qNuEM.2CG8M5t1IX/.K', 'admin', NULL, '2024-11-02 13:47:51', '2024-11-02 13:49:52'),
+(5, 'Andini', 'Andini@gmail.com', NULL, '$2y$10$OCWCUd5uDMwZaZu7j91VnejwmASexOdeciorJTpto1.EeLe3GVpyS', 'admin', NULL, '2024-11-14 09:46:34', '2024-11-20 21:09:07'),
+(6, 'dina', 'dina@gmail.com', NULL, '$2y$10$WawW7KQ5c5wH1/TCDrySZeEm6VAETD5t9tq7FrCIKxsUMyJ1XMMDK', 'admin', NULL, '2024-11-20 21:10:15', '2024-11-20 21:21:38'),
+(7, 'dini', 'dini@gmail.com', NULL, '$2y$10$7N6FDqXpC/jNjC2i5xMb/efFYDiKIlUm0t5W5iCm7FA6RpblzkI1i', 'admin', NULL, '2024-11-20 21:12:01', '2024-11-20 21:31:28'),
+(8, 'mama', 'mama@gmail.com', NULL, '$2y$10$5pGkPgxPuxRabPuXFsyyiee0beHUdiyQtQ0SUBnMYztukbY8KcYtW', 'user', NULL, '2024-11-20 21:17:22', '2024-11-20 21:17:22'),
+(9, 'sipa', 'sipa@gmail.com', NULL, '$2y$10$vhz02AM2A9WnXZyCOhMDKe5hZKzzPx23XIiot7xqcFYxo9086nIlK', 'user', NULL, '2024-11-20 21:23:10', '2024-11-20 21:23:10'),
+(10, 'Andini', 'andini16@gmail.com', NULL, '$2y$10$ylBlmEJz9kqpscaHrvsXSOHU0Q3RNTYAQ0R7RoutexSb4VXFeecTW', 'user', NULL, '2024-11-20 21:24:37', '2024-11-20 21:24:37'),
+(11, 'yusril', 'yusril@gmail.com', NULL, '$2y$10$tPKHM8tW8xBYKQ0imJ8V2eT7pGQplqsoHE3NtLADllyhIeX2xc6Bm', 'user', NULL, '2024-11-20 21:25:35', '2024-11-20 21:25:35'),
+(12, 'emoy', 'emoy@gmail.com', NULL, '$2y$10$GKYrFX/Tgg0mVQiI71OYhOllwbKwrGBfZtcEpHXhbE7gSwjcIwaE6', 'user', NULL, '2024-11-20 21:27:29', '2024-11-20 21:27:29'),
+(13, 'ama', 'ama@gmail.com', NULL, '$2y$10$2.rrRiu4N.Pvm.OrAPLwwe9UnWFKmDREBQlwu.7CkIiP8si65MPkW', 'user', NULL, '2024-11-20 21:32:13', '2024-11-20 21:32:13');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `agendas`
+--
+ALTER TABLE `agendas`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `comments`
+--
+ALTER TABLE `comments`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `comments_photo_id_foreign` (`photo_id`),
+  ADD KEY `comments_user_id_foreign` (`user_id`);
+
+--
+-- Indexes for table `failed_jobs`
+--
+ALTER TABLE `failed_jobs`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `failed_jobs_uuid_unique` (`uuid`);
+
+--
+-- Indexes for table `galleries`
+--
+ALTER TABLE `galleries`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `galleries_user_id_foreign` (`user_id`);
+
+--
+-- Indexes for table `infos`
+--
+ALTER TABLE `infos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `likes`
+--
+ALTER TABLE `likes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `migrations`
+--
+ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `password_resets`
+--
+ALTER TABLE `password_resets`
+  ADD KEY `password_resets_email_index` (`email`);
+
+--
+-- Indexes for table `personal_access_tokens`
+--
+ALTER TABLE `personal_access_tokens`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `personal_access_tokens_token_unique` (`token`),
+  ADD KEY `personal_access_tokens_tokenable_type_tokenable_id_index` (`tokenable_type`,`tokenable_id`);
+
+--
+-- Indexes for table `photos`
+--
+ALTER TABLE `photos`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `photos_gallery_id_foreign` (`gallery_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `users_email_unique` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `agendas`
+--
+ALTER TABLE `agendas`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `comments`
+--
+ALTER TABLE `comments`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `failed_jobs`
+--
+ALTER TABLE `failed_jobs`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `galleries`
+--
+ALTER TABLE `galleries`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `infos`
+--
+ALTER TABLE `infos`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `likes`
+--
+ALTER TABLE `likes`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `migrations`
+--
+ALTER TABLE `migrations`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `personal_access_tokens`
+--
+ALTER TABLE `personal_access_tokens`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `photos`
+--
+ALTER TABLE `photos`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `comments`
+--
+ALTER TABLE `comments`
+  ADD CONSTRAINT `comments_photo_id_foreign` FOREIGN KEY (`photo_id`) REFERENCES `photos` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `comments_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `galleries`
+--
+ALTER TABLE `galleries`
+  ADD CONSTRAINT `galleries_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `photos`
+--
+ALTER TABLE `photos`
+  ADD CONSTRAINT `photos_gallery_id_foreign` FOREIGN KEY (`gallery_id`) REFERENCES `galleries` (`id`) ON DELETE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
